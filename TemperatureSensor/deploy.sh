a4ed93aa213cf02c9d5df67d6415c771f0984a25
Michal Trnka <trnkamich@gmail.com>
2018-03-02 16:57:26 -0600

    debug deploy script

--- TemperatureSensor/deploy.sh
+++ TemperatureSensor/deploy.sh
@@ -1,13 +1,9 @@
 echo "deploying"
 eval `ssh-agent -s`
 pwd
-echo "next"
-ls
-echo "next"
-ls ../
-echo "next"
-ls ../../
-echo "next"
+echo "ls /temp"
+ls /temp/
+echo "adding key"
 ssh-add /tmp/deploy_rsa
 echo "ssh added"
 ssh pi@129.62.149.69 pwd
