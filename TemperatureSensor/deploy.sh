6d6ce485cd34cafadca85a1416675a919ed44634
Michal Trnka <trnkamich@gmail.com>
2018-03-02 17:00:00 -0600

    SSH connection from Travis

--- TemperatureSensor/deploy.sh
+++ TemperatureSensor/deploy.sh
@@ -1,9 +1,9 @@
-echo "deploying"
+echo "starting ssh agent"
 eval `ssh-agent -s`
-pwd
-echo "ls /temp"
-ls /temp/
-echo "adding key"
+echo "changing key rights"
+chmod 600 /tmp/deploy_rsa
+echo "adding ssh key"
 ssh-add /tmp/deploy_rsa
 echo "ssh added"
+echo "connecting to remote server"
 ssh pi@129.62.149.69 pwd
