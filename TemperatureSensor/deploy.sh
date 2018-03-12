7a54653be5f193aa59cbc38dec5234cd0fcfe43c
Michal Trnka <trnkamich@gmail.com>
2018-03-12 12:57:11 -0500

    Moved commands from bash file to travis file

--- TemperatureSensor/deploy.sh
+++ TemperatureSensor/deploy.sh
@@ -1,9 +1,2 @@
-echo "starting ssh agent"
-eval `ssh-agent -s`
-echo "changing key rights"
-chmod 600 /tmp/deploy_rsa
-echo "adding ssh key"
-ssh-add /tmp/deploy_rsa
-echo "ssh added"
 echo "connecting to remote server"
 ssh pi@129.62.149.69 pwd
