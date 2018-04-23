5bb84842f21a24ea0c3f617e7883dd6a567566f2
Michal Trnka <trnkamich@gmail.com>
2018-04-23 16:34:03 -0500

    Basic query debug

--- TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
+++ TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
@@ -2,7 +2,9 @@ package cz.michaltrnka.smarthome.lambda.temperature_history;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.TableCollection;
@@ -15,7 +17,7 @@ public class Handler implements RequestHandler<Object, String> {
     private final String DYNAMODB_TABLE_NAME = "temperature";
 
     public String handleRequest(Object input, Context context) {
-        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
         //client.setRegion(Region.getRegion(Regions.EU_CENTRAL_1));
         //DynamoDB dynamoDb = new DynamoDB(client);
         //TableCollection<ListTablesResult> tables = dynamoDb.listTables();
