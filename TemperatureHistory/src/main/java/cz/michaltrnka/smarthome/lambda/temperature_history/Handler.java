ec9d3f16c6e80c06ea80f71088d6976ea99fa5bf
Michal Trnka <trnkamich@gmail.com>
2018-04-24 15:28:08 -0500

    Changed result set to have 100 results distributed evenly across the time range.

--- TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
+++ TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
@@ -2,10 +2,7 @@ package cz.michaltrnka.smarthome.lambda.temperature_history;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.ItemCollection;
-import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -20,40 +17,91 @@ public class Handler implements RequestHandler<TemperatureRequest, String> {
     private final String DYNAMODB_TABLE_NAME = "temperature";
     private final String SENSOR_ID = "temperatureSensor";
     private final String VALUE_FIELD_NAME = "value";
+    private final int COUNT = 100;
+    private Table table;
 
     /**
      * Gets temperature history.
      * <p>
      * From and To parameters are inclusive,
      *
-     * @param input   Input to get temperatures from to date, including
+     * @param input Input to get temperatures from to date, including
      * @return JSON array of found temperature records
      */
     public String handleRequest(TemperatureRequest input, Context context) {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
         DynamoDB dynamoDb = new DynamoDB(client);
+        table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
 
-        long from = input.getFrom();
-        long to = input.getTo() == 0L ? new Date().getTime() : input.getTo();
-        Map<String, String> nameMap = new HashMap<String, String>();
-        nameMap.put("#t","timestamp");
-
-        QuerySpec querySpec = new QuerySpec()
-                .withKeyConditionExpression("sensor_id = :id and #t between :from and :to")
-                .withNameMap(nameMap)
-                .withValueMap(new ValueMap()
-                    .withString(":id",SENSOR_ID)
-                    .withLong(":from", from)
-                    .withLong(":to", to));
-
-        ItemCollection<QueryOutcome> items = dynamoDb.getTable(DYNAMODB_TABLE_NAME).query(querySpec);
+        long[] times = getTimes(input.getFrom(), input.getTo(), COUNT);
 
         JSONArray json = new JSONArray();
-        for(Item item : items){
-            json.put(item.getJSONPretty(VALUE_FIELD_NAME));
+        for (int i = 0; i < times.length; i++) {
+            json.put(getResultForTime(times[i]));
         }
 
         return json.toString();
     }
 
+    /**
+     * Get approximate times for sampling the time range for given amount of results
+     *
+     * @param from  time to start
+     * @param to    time to finish
+     * @param count count of results
+     * @return approximate times for results
+     */
+    public long[] getTimes(long from, long to, int count) {
+        if (to == 0) {
+            to = new Date().getTime();
+        }
+        if (to < from) {
+            long temp = to;
+            to = from;
+            from = temp;
+        }
+        long[] times = new long[count];
+        times[0] = from;
+        times[count - 1] = to;
+
+        if (count <= 2) {
+            return times;
+        }
+
+        long period = (to - from) / (count - 1);
+
+        for (int i = 1; i < count; i++) {
+            times[i] = from + i * period;
+        }
+
+        return times;
+    }
+
+    public String getResultForTime(long time) {
+        Map<String, String> nameMap = new HashMap<String, String>();
+        nameMap.put("#t", "timestamp");
+        QuerySpec querySpec = new QuerySpec()
+                .withKeyConditionExpression("sensor_id = :id and #t < :time")
+                .withNameMap(nameMap)
+                .withValueMap(new ValueMap()
+                        .withString(":id", SENSOR_ID)
+                        .withLong(":time", time))
+                .withMaxResultSize(1);
+
+        ItemCollection<QueryOutcome> items = table.query(querySpec);
+        //get the closest higher result
+        if (items.getAccumulatedItemCount() == 0) {
+            querySpec = new QuerySpec()
+                    .withKeyConditionExpression("sensor_id = :id and #t > :time")
+                    .withNameMap(nameMap)
+                    .withValueMap(new ValueMap()
+                            .withString(":id", SENSOR_ID)
+                            .withLong(":time", time))
+                    .withMaxResultSize(1);
+
+            items = table.query(querySpec);
+        }
+
+        return items.iterator().next().getJSONPretty(VALUE_FIELD_NAME);
+    }
 }
