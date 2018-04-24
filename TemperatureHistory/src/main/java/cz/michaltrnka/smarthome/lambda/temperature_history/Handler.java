f9bae023fbebb6565028ee1ca6272d731f5b95a7
Michal Trnka <trnkamich@gmail.com>
2018-04-24 13:36:04 -0500

    Adding filtering option - first test

--- TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
+++ TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/Handler.java
@@ -7,10 +7,19 @@ import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Object, String> {
+public class Handler implements RequestHandler<TemperatureRequest, String> {
     private final String DYNAMODB_TABLE_NAME = "temperature";
 
-    public String handleRequest(Object input, Context context) {
+    /**
+     * Gets temperature history.
+     *
+     * From and To parameters are inclusive,
+     *
+     * @param input Input to get temperatures from to date, including
+     * @param context
+     * @return
+     */
+    public String handleRequest(TemperatureRequest input, Context context) {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
 
         DynamoDB dynamoDb = new DynamoDB(client);
@@ -19,7 +28,7 @@ public class Handler implements RequestHandler<Object, String> {
 
 
         context.getLogger().log("Input: " + input);
-        String output = "Hello, " + input + "! + \n Lambda result: \n" + i.toJSONPretty();
+        String output = "Hello, " + input + "! + \n Input: \n" + input.getFrom().toString();
         return output;
     }
 
---
+++ TemperatureHistory/src/main/java/cz/michaltrnka/smarthome/lambda/temperature_history/TemperatureRequest.java
@@ -0,0 +1,24 @@
+package cz.michaltrnka.smarthome.lambda.temperature_history;
+
+import java.util.Date;
+
+public class TemperatureRequest {
+    private Date from;
+    private Date to;
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+}
